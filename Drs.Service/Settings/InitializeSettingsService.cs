5508f1a5aeb1b99d1b9a7048cf1a90cb068e2fde
Israel Vite Silva <israelvi@hotmail.com>
2015-09-29 19:14:18 -0500

    Faltantes

---
+++ Drs.Service/Settings/InitializeSettingsService.cs
@@ -0,0 +1,33 @@
+﻿using Drs.Model.Settings;
+using Drs.Repository.Address;
+using Drs.Repository.Setting;
+using Drs.Service.Configuration;
+
+namespace Drs.Service.Settings
+{
+    public static class InitializeSettingsService
+    {
+        public static void FullInitialize()
+        {
+            InitializeConstants();
+            InitializeControls();
+        }
+
+        public static void InitializeConstants()
+        {
+            var service = new SettingService(new SettingRepository());
+            var resConstants = service.FindAllSettings();
+            var dicSettings = resConstants.DicSettings;
+            SettingConfigure.InitConstants(dicSettings);
+        }
+
+        public static void InitializeControls()
+        {
+            var service = new SettingService(new SettingRepository());
+            var resControls = service.FindAllControlTitlesByLanguage(SettingsData.Language);
+            var lstAddressSetting = resControls.LstControls;
+            SettingConfigure.InitControls(lstAddressSetting);
+            SettingAddress.Initialize(new AddressRepository());
+        }
+    }
+}
