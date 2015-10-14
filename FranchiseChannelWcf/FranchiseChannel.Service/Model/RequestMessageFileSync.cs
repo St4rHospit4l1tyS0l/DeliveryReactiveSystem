3b49a60e2a2a9d0fd4e65eb43ad264ab95746a67
Israel Vite Silva <israelvi@hotmail.com>
2015-10-14 16:42:13 -0500

    Sync ready to server

---
+++ FranchiseChannelWcf/FranchiseChannel.Service/Model/RequestMessageFileSync.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace FranchiseChannel.Service.Model
+{
+    [MessageContract]
+    public class RequestMessageFileSync
+    {
+        [MessageBodyMember]
+        public Guid UidVersion { get; set; }
+
+        [MessageBodyMember]
+        public String FileName { get; set; }
+    }
+}
---
+++ FranchiseChannelWcf/FranchiseChannel.Service/Model/ResponseMessageFileSync.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.ServiceModel;
+
+namespace FranchiseChannel.Service.Model
+{
+    [MessageContract]
+    public class ResponseMessageFileSync
+    {
+        [MessageHeader(MustUnderstand = true)]
+        public string Message { get; set; }
+
+        [MessageHeader(MustUnderstand = true)]
+        public bool HasError { get; set; }
+
+        [MessageBodyMember(Order = 1)]
+        public Stream File { get; set; }
+    }
+}
