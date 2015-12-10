a5df8b41fdb8f32b2750c75492c75fc25724b648
Israel Vite Silva <israelvi@hotmail.com>
2015-12-10 16:49:48 -0600

    Modificaciones para los reportes

---
+++ CentralManagement/Areas/Report/Controllers/SaleController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Drs.Model.Constants;
+
+namespace CentralManagement.Areas.Report.Controllers
+{
+    [Authorize(Roles = RoleConstants.MANAGER + ", " + RoleConstants.STORE_MANAGER)]
+    public class SaleController : Controller
+    {
+        // GET: Report/Sale
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
