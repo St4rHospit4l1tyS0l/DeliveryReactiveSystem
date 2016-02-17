b572368bd3e239b389cbdacf860d4b3eae37e509
Israel Vite Silva <israelvi@hotmail.com>
2016-02-17 17:48:56 -0600

    Reporte de ventas diarias

---
+++ CentralManagement/Models/ReportsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Drs.Model.Report;
+
+namespace CentralManagement.Models
+{
+    public class ReportsViewModel
+    {
+        
+    }
+
+    public class DailySalesReportModel
+    {
+        public IEnumerable<DailySaleModel> Sales { get; set; }
+    }
+}
--- Drs.Repository/Report/ReportRepository.cs
+++ Drs.Repository/Report/ReportRepository.cs
@@ -21,26 +21,18 @@ namespace Drs.Repository.Report
         }
         public IEnumerable<DailySaleModel> GetDailySaleInfo(DateTime startDate, DateTime endDate)
         {
-            //string startDateString = "08/12/2015 00:00:00 AM";
-            //string endDateString = "08/12/2015 15:09:00 PM";
-            //DateTime startDate = DateTime.Parse(startDateString,
-            //                          System.Globalization.CultureInfo.InvariantCulture); 
-            //DateTime endDate = DateTime.Parse(endDateString,
-            //                          System.Globalization.CultureInfo.InvariantCulture);
             return DbEntities.Daily_Sales_group_by_date(startDate, endDate)
-                    .Where(e => e.SaleDate >= startDate && e.SaleDate <= endDate)
-                    .Select(x => new DailySaleModel { 
-                        SaleDate  = x.SaleDate.ToString(), 
-                        SubTotal = (decimal) x.Subtotal, 
-                        Tax = (decimal) x.Taxes, 
-                        Total = (decimal) x.Total})
+                    //.Where(e => e.SaleDate >= startDate && e.SaleDate <= endDate)
+                    .Select(x => x.Total != null ? new DailySaleModel { 
+                        month  = x.SaleDate, 
+                        sales = (decimal) x.Total} : null)
                     .ToList();    
         }
 
         public IEnumerable<AgentSalesModel> GetAgentSaleInfo(DateTime startDate, DateTime endDate)
         {
             return DbEntities.User_Sales_group_by_date(startDate, endDate)
-                .Where(e => e.Column1 >= startDate && e.Column1 <= endDate)
+                .Where(e => e.SaleDate >= startDate && e.SaleDate <= endDate)
                 .Select(e => new AgentSalesModel
                 {
                     AgentName = e.UserName,
