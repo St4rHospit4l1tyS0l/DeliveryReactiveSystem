2e56944dcbd74d44fe0d15e875b38443f72ddb4c
Israel Vite Silva <israelvi@hotmail.com>
2016-05-26 13:45:21 -0500

    Mejoras

---
+++ Drs.Infrastructure/Logging/InternalLogger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Script.Serialization;
+using Drs.Infrastructure.Resources;
+using log4net;
+
+namespace Drs.Infrastructure.Logging
+{
+    public static class InternalLogger
+    {
+        private static readonly log4net.ILog Log = LogManager.GetLogger(typeof(InternalLogger));
+
+
+        public static void WriteException(Exception ex, params object[] arrVal)
+        {
+            var msgException = ex.Message;
+            var exceptionLogUid = Guid.NewGuid();
+            var innerException = GetInnerExceptions(ex);
+            var paramsValues = GetSerializedValues(arrVal);
+            var stackTrace = ex.StackTrace;
+            Log.ErrorFormat("Uid: {0} | Msg: {1} | InExc: {2} | Params: {3} | St: {4}", exceptionLogUid, msgException, innerException, paramsValues, stackTrace);
+        }
+
+        public static string GetInnerExceptions(Exception exception)
+        {
+            try
+            {
+                if (exception.InnerException == null)
+                {
+                    return "|EOE|";
+                }
+                return string.Format("|{0}|{1}", exception.InnerException.Message, GetInnerExceptions(exception.InnerException));
+            }
+            catch (Exception)
+            {
+                return string.Format(ResShared.ERROR_RECURSION, "GetInnerExceptions");
+            }
+        }
+
+
+        public static string GetSerializedValues(object[] arrVal)
+        {
+            try
+            {
+                dynamic serializer = new JavaScriptSerializer();
+                return serializer.Serialize(arrVal);
+            }
+            catch (Exception)
+            {
+                return string.Format(ResShared.ERROR_RECURSION, "GetSerializedValues");
+            }
+        }
+
+    }
+}
---
+++ Drs.Ui/Gmap/HtmlInteropService.cs
@@ -0,0 +1,32 @@
+﻿using Drs.ViewModel.Order;
+using MahApps.Metro.Controls;
+using Newtonsoft.Json;
+
+namespace Drs.Ui.Gmap
+{
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    public class HtmlInteropService
+    {
+        private readonly Flyout _control;
+
+        public HtmlInteropService(Flyout control)
+        {
+            _control = control;
+        }
+
+        public object GetInfoAddress()
+        {
+            var mv = _control.DataContext as UpsertAddressFoVm;
+
+            if (mv == null)
+                return "Configuración errónea";
+
+            return JsonConvert.SerializeObject(new
+            {
+                mv.Controls,
+                mv.Franchise.LastConfig,
+                mv.Franchise.StoresCoverage
+            });
+        }
+    }
+}
